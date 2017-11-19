a70bed3f7cb71d224c6adaf0105fe86759cd41b0
NivenT <nachenjang@gmail.com>
2017-11-19 04:57:47 -0800

    made key mappings not stupid

--- chip8/config.py
+++ chip8/config.py
@@ -21,22 +21,22 @@ PROGRAM_COUNTER_START = 0x200
 
 # Sets which keys on the keyboard map to the Chip 8 keys
 KEY_MAPPINGS = {
-    0x0: pygame.K_KP0,
-    0x1: pygame.K_KP1,
-    0x2: pygame.K_KP2,
-    0x3: pygame.K_KP3,
-    0x4: pygame.K_KP4,
-    0x5: pygame.K_KP5,
-    0x6: pygame.K_KP6,
-    0x7: pygame.K_KP7,
-    0x8: pygame.K_KP8,
-    0x9: pygame.K_KP9,
-    0xA: pygame.K_a,
-    0xB: pygame.K_b,
-    0xC: pygame.K_c,
-    0xD: pygame.K_d,
-    0xE: pygame.K_e,
-    0xF: pygame.K_f,
+    0x0: pygame.K_x,
+    0x1: pygame.K_1,
+    0x2: pygame.K_2,
+    0x3: pygame.K_3,
+    0x4: pygame.K_q,
+    0x5: pygame.K_w,
+    0x6: pygame.K_e,
+    0x7: pygame.K_a,
+    0x8: pygame.K_s,
+    0x9: pygame.K_d,
+    0xA: pygame.K_z,
+    0xB: pygame.K_c,
+    0xC: pygame.K_4,
+    0xD: pygame.K_r,
+    0xE: pygame.K_f,
+    0xF: pygame.K_v,
 }
 
 # The font file to use
